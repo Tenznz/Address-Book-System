1e25e9386475c0c18fdbba6e8696e1fe18dac118
Duk <dhugkar@gmail.com>
2021-11-10 00:26:32 +0530

    Created Contact.java

--- src/com/bridgelabz/address_book/AddressBook.java
+++ src/com/bridgelabz/address_book/AddressBook.java
@@ -3,7 +3,6 @@ package com.bridgelabz.address_book;
 public class AddressBook {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Welcome to Address Book");	
+		Contact contact=new Contact();
 	}
 }
---
+++ src/com/bridgelabz/address_book/Contact.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.address_book;
+
+import java.util.Scanner;
+
+public class Contact {
+
+	Scanner sc = new Scanner(System.in);
+
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private String state;
+	private int zip;
+	private long phoneNumber;
+	private String email;
+	@Override
+	public String toString() {
+		return "Contact [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city=" + city
+				+ ", state=" + state + ", zip=" + zip + ", phoneNumber=" + phoneNumber + ", email=" + email + "]";
+	}
+
+}
